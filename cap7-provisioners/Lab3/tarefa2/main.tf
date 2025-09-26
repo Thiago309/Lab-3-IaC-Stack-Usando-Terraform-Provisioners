413436fc587770b6da95a617c670bb78cfa33192
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2025-09-26 13:41:04 -0300

    tarefa2 update main.tf
    Anotações complementares para o aprendizado.

--- cap7-provisioners/Lab3/tarefa2/main.tf
+++ cap7-provisioners/Lab3/tarefa2/main.tf
@@ -4,17 +4,26 @@ resource "aws_instance" "dsa_instance" {
   
   instance_type = var.instance_type
   
+  # Pôr o mesmo nome do par de chaves criada no ambiente AWS. Para poder executar
+  # as credencias e realizar a conexão com a instância EC2.
   key_name = "dsa-lab3"
 
   tags = {
     Name = "lab3-t2-terraform"
   }
 
+  # Criando um provisioner do tipo remote-exec, para realizar uma conexão SSH e 
+  # executar um script ou um conjunto de comandos em um recurso remoto
   provisioner "remote-exec" {
     
+    # Bloco 1: Lista de comandos shell linux.
+            # 1º Comando: Atualiza o Sistema Operacional.
     inline = ["sudo yum update -y", 
+            # 2º Comando: Instala o Apache 2 (httpd) que é um serviço web.
               "sudo yum install httpd -y", 
+            # 3º Comando: Inicia o Apache 2 (httpd).
               "sudo systemctl start httpd", 
+            # 4º Comando: Executa o bash dentro da instância EC2 e executa o comando echo para criar a frase abaixo, e salvar dentro do arquivo html.
               "sudo bash -c 'echo Criando o Primeiro Web Server com Terraform na DSA > /var/www/html/index.html'"]
 
     connection {
