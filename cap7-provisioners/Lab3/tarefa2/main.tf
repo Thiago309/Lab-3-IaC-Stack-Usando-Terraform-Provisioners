89825393ef0e06e3a85f3880d199ba49e798909a
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2025-09-26 13:58:17 -0300

    tarefa2 provisioners
    ultimas configurações.

--- cap7-provisioners/Lab3/tarefa2/main.tf
+++ cap7-provisioners/Lab3/tarefa2/main.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "dsa_instance" {
   
   instance_type = var.instance_type
   
-  # Pôr o mesmo nome do par de chaves criada no ambiente AWS. Para poder executar
-  # as credencias e realizar a conexão com a instância EC2.
+  # Pôr o mesmo nome do par de chaves criada no ambiente AWS para poder realizar a associação
+  # dessas credencias com a chave privada (dsa-lab3.pem) e realizar a conexão com a instância EC2.
   key_name = "dsa-lab3"
 
   tags = {
@@ -26,10 +26,16 @@ resource "aws_instance" "dsa_instance" {
             # 4º Comando: Executa o bash dentro da instância EC2 e executa o comando echo para criar a frase abaixo, e salvar dentro do arquivo html.
               "sudo bash -c 'echo Criando o Primeiro Web Server com Terraform na DSA > /var/www/html/index.html'"]
 
+    # Bloco 2: Realizar conexão com a instância via SSH.
     connection {
+      # Tipo de conexão.
       type     = "ssh"
+      # O tipo de usuario depende da AMI da instância que você está utilizando. Nesse exemplo estou utilizando para a (ami-0a0d9cf81c479446a). 
+      # Caso queira mudar de usuario é aconselhavel consultar a documentação das AMI's no site de AWS. 
       user     = "ec2-user"
+      # Indica o arquivo com a chave privada para realizar a conexão com a EC2.
       private_key = file("dsa-lab3.pem")
+      # Endereço para realizar a conexão com a instância EC2. Recebe o valor do IP atraves da função output "public_ip" que está no arquivo outputs.tf .
       host     = self.public_ip
     }
   }
