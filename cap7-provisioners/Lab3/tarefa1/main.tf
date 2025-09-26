c6868a2285f4c65ca34315c4cafed2a4e35cdc8b
Thiago Vinicius <116599158+Thiago309@users.noreply.github.com>
2025-09-26 12:27:15 -0300

    tarefa1

--- cap7-provisioners/Lab3/tarefa1/main.tf
+++ cap7-provisioners/Lab3/tarefa1/main.tf
@@ -1,3 +1,5 @@
+
+# "serviço que vou utilizar na aws" "nome da instancia a ser criada"
 resource "aws_instance" "dsa_instance" {
   
   ami = "ami-0a0d9cf81c479446a"  
@@ -8,7 +10,9 @@ resource "aws_instance" "dsa_instance" {
     Name = "lab3-t1-terraform"
   }
 
+# (palavra reservada) (tipo de provisioner)
   provisioner "local-exec" {
+    # echo é o comando que imprime na tela. O $ retorna o valor da variavel. Salva o valor em um arquivo .txt
     command = "echo ${aws_instance.dsa_instance.public_ip} > ip_dsa_instance.txt"
   }
 }
